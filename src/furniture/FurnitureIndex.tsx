a8e7a77f5e6365314324f1663910faa02ad4541f
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-06-09 05:53:03 +0900

    가구 커스텀 메인

--- src/furniture/FurnitureIndex.tsx
+++ src/furniture/FurnitureIndex.tsx
@@ -1,6 +1,9 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Provider } from "react-redux";
 import { ControlTap } from "./interface/control/ControlTap";
+import { ModelIndex } from "./models/ModelIndex";
+import { store } from "./modules/store";
 
 export const FurnitureIndex = () => {
   return (
@@ -11,13 +14,14 @@ export const FurnitureIndex = () => {
           fov: 60,
           far: 3000,
           near: 3,
+          position: [30, 30, 30],
         }}>
-        <ambientLight intensity={0.5} />
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshNormalMaterial />
-        </mesh>
-        <OrbitControls />
+        <Provider store={store}>
+          <ambientLight intensity={0.5} />
+          <ModelIndex />
+
+          <OrbitControls />
+        </Provider>
       </Canvas>
     </>
   );
