bf27616e71252a54062161863887b55467c4a906
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-06-09 05:54:13 +0900

    가구 스토어

--- src/furniture/modules/furniture.tsx
+++ src/furniture/modules/furniture.tsx
@@ -1,54 +1,92 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-type SliceState = {
-  name: string;
-  initialState: object;
+type ChairColumnType = {
+  select: string;
+  material: string;
+  newChair: boolean;
+  chairColumn: Array<number>;
+  newSofa: boolean;
+  newDesk: boolean;
+  chairWidth: number;
+  height: number;
+  side: number;
+};
+
+const initialState: ChairColumnType = {
+  select: "",
+  material: "",
+  newChair: false,
+  chairColumn: [],
+  newSofa: false,
+  newDesk: false,
+  chairWidth: 10,
+  height: 10,
+  side: 10,
 };
 
 export const furnitureSlice = createSlice({
   name: "furniture",
-  initialState: { value: "" },
-  reducers: {},
+  initialState: initialState,
+  reducers: {
+    selectChange: (state, action) => {
+      state.select = action.payload;
+    },
+    setChairWidth: (state, action) => {
+      state.chairWidth = action.payload;
+    },
+    setNewChair: (state, action) => {
+      state.newChair = action.payload;
+    },
+    setChairColumn: (state, action) => {
+      console.log(state.chairColumn);
+      state.chairColumn = [...state.chairColumn, action.payload];
+      console.log(state.chairColumn);
+      console.log(action);
+      //state.chairColumn.push(action.payload);
+    },
+  },
 });
 
-const SOFA = "furniture/SOFA" as const;
-const CHAIR = "furniture/CHAIR" as const;
-const DESK = "furniture/DESK" as const;
+export const { selectChange, setChairWidth, setNewChair, setChairColumn } = furnitureSlice.actions;
 
-export const chengeSofa = () => ({
-  type: SOFA,
-});
-export const chengeChair = () => ({
-  type: CHAIR,
-});
-export const chengeDesk = () => ({
-  type: DESK,
-});
+// const SOFA = "furniture/SOFA" as const;
+// const CHAIR = "furniture/CHAIR" as const;
+// const DESK = "furniture/DESK" as const;
 
-type FurnitureAction =
-  | ReturnType<typeof chengeSofa>
-  | ReturnType<typeof chengeChair>
-  | ReturnType<typeof chengeDesk>;
+// export const chengeSofa = () => ({
+//   type: SOFA,
+// });
+// export const chengeChair = () => ({
+//   type: CHAIR,
+// });
+// export const chengeDesk = () => ({
+//   type: DESK,
+// });
 
-type FurnitureState = {
-  select: string;
-};
+// type FurnitureAction =
+//   | ReturnType<typeof chengeSofa>
+//   | ReturnType<typeof chengeChair>
+//   | ReturnType<typeof chengeDesk>;
 
-const initialState: FurnitureState = {
-  select: "",
-};
+// type FurnitureState = {
+//   select: string;
+// };
 
-function furniture(state: FurnitureState = initialState, action: FurnitureAction): FurnitureState {
-  switch (action.type) {
-    case SOFA:
-      return { select: "SOFA" };
-    case CHAIR:
-      return { select: "CHAIR" };
-    case DESK:
-      return { select: "DESK" };
-    default:
-      return state;
-  }
-}
+// const initialState: FurnitureState = {
+//   select: "",
+// };
 
-export default furniture;
+// function furniture(state: FurnitureState = initialState, action: FurnitureAction): FurnitureState {
+//   switch (action.type) {
+//     case SOFA:
+//       return { select: "SOFA" };
+//     case CHAIR:
+//       return { select: "CHAIR" };
+//     case DESK:
+//       return { select: "DESK" };
+//     default:
+//       return state;
+//   }
+// }
+
+// export default furniture;
