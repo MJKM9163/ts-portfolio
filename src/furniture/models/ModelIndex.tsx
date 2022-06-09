9ef275983d84b5233fe3ed9fae93c2336a04ac7c
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-06-10 04:31:58 +0900

    코드 수정

--- src/furniture/models/ModelIndex.tsx
+++ src/furniture/models/ModelIndex.tsx
@@ -1,13 +1,9 @@
-import { RootState } from "../modules/store";
-import { useSelector } from "react-redux";
-import { ChairIndex } from "./Chair";
+import { ChairField } from "./Chair";
 
 export const ModelIndex = () => {
-  const select = useSelector((state: RootState) => state.furniture.chairWidth);
-  console.log(select);
   return (
     <group>
-      <ChairIndex />
+      <ChairField />
     </group>
   );
 };
