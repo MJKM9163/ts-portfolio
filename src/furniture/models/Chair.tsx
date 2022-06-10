30325694109136bf6d8e78097537a7d8b8c42416
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-06-11 04:40:50 +0900

    기둥 마우스 이벤트, rotation 추가

--- src/furniture/models/Chair.tsx
+++ src/furniture/models/Chair.tsx
@@ -1,27 +1,44 @@
-import { ThreeEvent } from "@react-three/fiber";
+import { ThreeEvent, useFrame } from "@react-three/fiber";
 import { createColumn } from "furniture/modules/furniture";
 import { RootState } from "furniture/modules/store";
 import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Color, Object3D, Vector3 } from "three";
+import { Color, Vector3 } from "three";
 
 const Column = () => {
   const ref = useRef<THREE.Mesh>(null);
-  const { chairColumn, height, width } = useSelector(
+  const { chairColumn, height, width, rotation } = useSelector(
     (state: RootState) => state.furniture.chairOption.column
   );
 
   return (
     <group>
       {chairColumn.map((i, columnIndex) => (
-        <mesh
-          position={new Vector3(i[0], i[1] - height[columnIndex] / 2, i[2])}
-          key={"column" + columnIndex}>
-          <cylinderGeometry
-            args={[width[columnIndex], width[columnIndex], height[columnIndex], 24]}
-          />
-          <meshNormalMaterial />
-        </mesh>
+        <group
+          key={"columnBox" + columnIndex}
+          position={new Vector3(i[0], i[1], i[2])}
+          rotation={[rotation[columnIndex][0], rotation[columnIndex][1], rotation[columnIndex][2]]}>
+          <mesh
+            onClick={(e: any) => {
+              e.stopPropagation();
+              console.log(e);
+            }}
+            onPointerOver={(e: any) => {
+              e.stopPropagation();
+              e.object.material.color.set(new Color(0, 1, 0.5));
+            }}
+            onPointerOut={(e: any) => {
+              e.stopPropagation();
+              e.object.material.color.set(new Color(0, 0, 1));
+            }}
+            position={new Vector3(0, i[1] - height[columnIndex] / 2, 0)}
+            key={"column" + columnIndex}>
+            <cylinderGeometry
+              args={[width[columnIndex], width[columnIndex], height[columnIndex], 24]}
+            />
+            <meshBasicMaterial color={"blue"} />
+          </mesh>
+        </group>
       ))}
     </group>
   );
@@ -29,32 +46,39 @@ const Column = () => {
 
 export const ChairField = () => {
   const posArray = new Array(10).fill(0);
-  //const ref = useRef<THREE.Mesh>(null);
   const check = useSelector((state: RootState) => state.furniture.chairOption.newChair);
   const dispatch = useDispatch();
+
+  const guide = useSelector((state: RootState) => state.furniture.guide);
+  const ref = useRef<THREE.Group>(null);
+
   return (
     <group>
       {check ? (
-        <group>
+        <group ref={ref}>
           {posArray.map((x, indexX) => (
             <group key={"X" + indexX}>
               {posArray.map((z, indexZ) => (
                 <mesh
-                  position={[indexX - 5, 0, indexZ - 5]}
+                  position={[indexX - 4.5, 0, indexZ - 4.5]}
                   key={"Z" + indexZ}
                   onClick={(e) => {
                     e.stopPropagation();
-                    const { x, y, z } = e.eventObject.position;
-                    dispatch(createColumn([[x, y, z], [0.5], [8]]));
-                    console.log([x, y, z]);
+                    if (guide) {
+                      const { x, y, z } = e.object.position;
+                      dispatch(createColumn([[x, y, z], [0.5], [8], [0, 0, 0]]));
+                    } else {
+                      const { x, z } = e.point;
+                      dispatch(createColumn([[x, 0, z], [0.5], [8], [0, 0, 0]]));
+                    }
                   }}
                   onPointerOver={(e: ThreeEvent<PointerEvent> | any) => {
                     e.stopPropagation();
-                    e.eventObject.material.color.set(new Color(1, 2.5, 1));
+                    if (guide) e.object.material.color.set(new Color(1, 2.5, 1));
                   }}
                   onPointerOut={(e: ThreeEvent<PointerEvent> | any) => {
                     e.stopPropagation();
-                    e.eventObject.material.color.set(new Color(0, 1, 1));
+                    e.object.material.color.set(new Color(0, 1, 1));
                   }}>
                   <boxGeometry args={[1, 1, 1]} />
                   <meshBasicMaterial color={"Cyan"} />
