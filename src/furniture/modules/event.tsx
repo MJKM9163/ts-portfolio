4ba9c0fec5e7e051249a80e93556d044d0586bde
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-06-14 06:38:38 +0900

    회전 단축키 추가

--- src/furniture/modules/event.tsx
+++ src/furniture/modules/event.tsx
@@ -1,15 +1,43 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setGuide } from "./furniture";
+import { setChairRotation, setGuide } from "./furniture";
 import { RootState } from "./store";
 
 export const Event = () => {
   const guide = useSelector((state: RootState) => state.furniture.guide);
+  const { select, rotation } = useSelector(
+    (state: RootState) => state.furniture.chairOption.column
+  );
   const dispatch = useDispatch();
   const keyEvent = (e: KeyboardEvent) => {
     if (e.key === "Shift") {
-      console.log(guide);
       dispatch(setGuide(!guide));
+    } else if (select !== -1) {
+      if (e.key === "q") {
+        if (rotation[select][0] + 0.1 < 5) {
+          dispatch(setChairRotation([rotation[select][0] + 0.1, select, 0]));
+        }
+      } else if (e.key === "w") {
+        if (rotation[select][1] + 0.1 < 5) {
+          dispatch(setChairRotation([rotation[select][1] + 0.1, select, 1]));
+        }
+      } else if (e.key === "e") {
+        if (rotation[select][2] + 0.1 < 5) {
+          dispatch(setChairRotation([rotation[select][2] + 0.1, select, 2]));
+        }
+      } else if (e.key === "a") {
+        if (rotation[select][0] - 0.1 > -5) {
+          dispatch(setChairRotation([rotation[select][0] - 0.1, select, 0]));
+        }
+      } else if (e.key === "s") {
+        if (rotation[select][1] - 0.1 > -5) {
+          dispatch(setChairRotation([rotation[select][1] - 0.1, select, 1]));
+        }
+      } else if (e.key === "d") {
+        if (rotation[select][2] - 0.1 > -5) {
+          dispatch(setChairRotation([rotation[select][2] - 0.1, select, 2]));
+        }
+      }
     }
   };
 
