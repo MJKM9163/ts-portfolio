19590aa196f4fe27aee950faea284de51fa7245b
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-06-15 23:04:16 +0900

    타입 가드 설정

--- src/furniture/modules/event.tsx
+++ src/furniture/modules/event.tsx
@@ -12,30 +12,32 @@ export const Event = () => {
   const keyEvent = (e: KeyboardEvent) => {
     if (e.key === "Shift") {
       dispatch(setGuide(!guide));
-    } else if (select !== -1) {
-      if (e.key === "q") {
-        if (rotation[select][0] + 0.1 < 5) {
-          dispatch(setChairRotation([rotation[select][0] + 0.1, select, 0]));
-        }
-      } else if (e.key === "w") {
-        if (rotation[select][1] + 0.1 < 5) {
-          dispatch(setChairRotation([rotation[select][1] + 0.1, select, 1]));
-        }
-      } else if (e.key === "e") {
-        if (rotation[select][2] + 0.1 < 5) {
-          dispatch(setChairRotation([rotation[select][2] + 0.1, select, 2]));
-        }
-      } else if (e.key === "a") {
-        if (rotation[select][0] - 0.1 > -5) {
-          dispatch(setChairRotation([rotation[select][0] - 0.1, select, 0]));
-        }
-      } else if (e.key === "s") {
-        if (rotation[select][1] - 0.1 > -5) {
-          dispatch(setChairRotation([rotation[select][1] - 0.1, select, 1]));
-        }
-      } else if (e.key === "d") {
-        if (rotation[select][2] - 0.1 > -5) {
-          dispatch(setChairRotation([rotation[select][2] - 0.1, select, 2]));
+    } else if (select !== false) {
+      if (typeof select === "number") {
+        if (e.key === "q") {
+          if (rotation[select][0] + 0.1 < 5) {
+            dispatch(setChairRotation([rotation[select][0] + 0.1, select, 0]));
+          }
+        } else if (e.key === "w") {
+          if (rotation[select][1] + 0.1 < 5) {
+            dispatch(setChairRotation([rotation[select][1] + 0.1, select, 1]));
+          }
+        } else if (e.key === "e") {
+          if (rotation[select][2] + 0.1 < 5) {
+            dispatch(setChairRotation([rotation[select][2] + 0.1, select, 2]));
+          }
+        } else if (e.key === "a") {
+          if (rotation[select][0] - 0.1 > -5) {
+            dispatch(setChairRotation([rotation[select][0] - 0.1, select, 0]));
+          }
+        } else if (e.key === "s") {
+          if (rotation[select][1] - 0.1 > -5) {
+            dispatch(setChairRotation([rotation[select][1] - 0.1, select, 1]));
+          }
+        } else if (e.key === "d") {
+          if (rotation[select][2] - 0.1 > -5) {
+            dispatch(setChairRotation([rotation[select][2] - 0.1, select, 2]));
+          }
         }
       }
     }
